29322f7cd68d0730dc932a8d669fe301ba036c90
SiddiquiSaab04 <devlaiba04@gmail.com>
2026-02-18 00:56:16 +0500

    validation in request leave

--- src/services/leave.service.ts
+++ src/services/leave.service.ts
@@ -8,16 +8,30 @@ export class LeaveService {
             const user = await prisma.user.findUnique({
                 where: { id: userId }
             });
+            const startDate = new Date(leaveData?.startDate ?? new Date());
+            const endDate = new Date(leaveData?.endDate ?? new Date());
+            const today = new Date();
+            today.setHours(0, 0, 0, 0);
+
+            if (startDate < today || endDate < today) {
+                throw new Error("Start and end dates must be greater than or equal to today's date");
+            }
+            if (startDate > endDate) {
+                throw new Error("Start date must be less than or equal to end date");
+            }
+            const diffTime = endDate.getTime() - startDate.getTime();
+            const days = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
+
             if (user) {
                 const leaves = await prisma.leave.create({
                     data: {
                         userId,
                         type: leaveData.type,
                         status: leaveData?.status || "PENDING",
-                        days: leaveData?.days || 1,
+                        days: days,
                         reason: leaveData?.reason || "No reason provided",
-                        startDate: leaveData?.startDate || new Date(),
-                        endDate: leaveData?.endDate || new Date(),
+                        startDate: startDate,
+                        endDate: endDate,
                     }
                 })
                 return leaves;
